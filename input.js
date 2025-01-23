8475652dd3aa4c4522139178c7307ccfb5c0e88d
Emily Todt <emilybtodt@gmail.com>
2025-01-23 10:22:19 -0500

    Add Stephanie as user

--- input.js
+++ input.js
@@ -10,6 +10,7 @@ document.getElementById("login-form").addEventListener("submit", function (event
       Daniel: { password: "12345", role: 1 }, // Access all environments
       Julie: { password: "54321", role: 3 }, // Auto-load production
       Emily: { password: "67890", role: 2 }, // Access Test and Production
+      Stephanie: { password: "56789", role: 2 }, // Access Test and Production
     };
 
     // Check credentials
